8c4d8e96c18a85e6b520ae91f557bc9456aacc05
arshamalh <arshamalh@gmail.com>
2022-06-01 18:54:42 +0430

    Button added.

---
+++ src/components/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let text = "";
+</script>
+
+<button type="submit">{text}</button>
+
+<style>
+  button {
+    color: #fff;
+    border: none;
+    border-radius: 8px;
+    color: #fff;
+    width: 100px;
+    height: 40px;
+    cursor: pointer;
+    background-color: #202142;
+    transition: 0.2s;
+  }
+
+  button:hover {
+    background-color: #1f3092;
+  }
+</style>
